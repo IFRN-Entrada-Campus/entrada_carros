794043122830eb1d1884bbd242ca0cc4c29062a5
kadetete <kadetesan@gmail.com>
2023-06-26 16:45:12 -0300

    configuração dos logs

--- backend/dbentrada.sql
+++ backend/dbentrada.sql
@@ -1,140 +1,263 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
 --
--- Host: 127.0.0.1:3306
--- Tempo de geração: 22/06/2023 às 12:48
--- Versão do servidor: 10.4.28-MariaDB
--- Versão do PHP: 8.2.4
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: 127.0.0.1    Database: dbentrada
+-- ------------------------------------------------------
+-- Server version	8.0.33
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Banco de dados: `dbentrada`
+-- Table structure for table `aluno`
 --
 
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `aluno`
---
-
+DROP TABLE IF EXISTS `aluno`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `aluno` (
-  `matriculaAluno` bigint(14) NOT NULL,
-  `noAluno` varchar(100) NOT NULL
+  `matriculaAluno` bigint NOT NULL,
+  `noAluno` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`matriculaAluno`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `log_alunoinsert` AFTER INSERT ON `aluno` FOR EACH ROW BEGIN
+  INSERT INTO logs (operacao, usuario, dataoperacao, detalhe)
+  VALUES ('INSERT', USER(), NOW(), CONCAT('Registro afetado: ', NEW.matriculaAluno, ', tabela aluno'));
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `log_alunoupdate` AFTER UPDATE ON `aluno` FOR EACH ROW BEGIN
+  INSERT INTO logs (operacao, usuario, dataoperacao, detalhe)
+  VALUES ('UPDATE', USER(), NOW(), CONCAT('Registro afetado: ', OLD.matriculaAluno, '-', NEW.matriculaAluno, ', tabela aluno'));
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `log_alunodelete` AFTER DELETE ON `aluno` FOR EACH ROW BEGIN
+  INSERT INTO logs (operacao, usuario, dataoperacao, detalhe)
+  VALUES ('DELETE', USER(), NOW(), CONCAT('Registro afetado: ', OLD.matriculaAluno, ', tabela aluno'));
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
--- Estrutura para tabela `carro`
+-- Table structure for table `carro`
 --
 
+DROP TABLE IF EXISTS `carro`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `carro` (
-  `idCarro` int(11) NOT NULL,
-  `marcaCarro` varchar(50) NOT NULL,
-  `modeloCarro` varchar(80) NOT NULL,
-  `anoCarro` int(11) NOT NULL,
+  `idCarro` int NOT NULL AUTO_INCREMENT,
+  `marcaCarro` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `modeloCarro` varchar(80) COLLATE utf8mb4_general_ci NOT NULL,
+  `anoCarro` int NOT NULL,
   `validaCnh` tinyint(1) NOT NULL,
-  `codigoEtiqueta` varchar(50) NOT NULL,
-  `matriculaRel` bigint(14) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
+  `codigoEtiqueta` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `matriculaRel` bigint NOT NULL,
+  PRIMARY KEY (`idCarro`),
+  KEY `fk_matricula` (`matriculaRel`),
+  CONSTRAINT `fk_matricula` FOREIGN KEY (`matriculaRel`) REFERENCES `aluno` (`matriculaAluno`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `log_carroinsert` AFTER INSERT ON `carro` FOR EACH ROW INSERT INTO logs (operacao, usuario, dataoperacao, detalhe)
+VALUES ('INSERT', USER(), NOW(), CONCAT('Registro afetado: ', NEW.idCarro, ', tabela carro')) */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `log_carroupdate` AFTER UPDATE ON `carro` FOR EACH ROW INSERT INTO logs (operacao, usuario, dataoperacao, detalhe)
+VALUES ('UPDATE', USER(), NOW(), CONCAT('Registro afetado: ', OLD.idCarro, '-', NEW.idCarro, ', tabela carro')) */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `log_carrodelete` AFTER DELETE ON `carro` FOR EACH ROW BEGIN
+  INSERT INTO logs (operacao, usuario, dataoperacao, detalhe)
+  VALUES ('DELETE', USER(), NOW(), CONCAT('Registro afetado: ', OLD.idCarro, ', tabela carro'));
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
--- Estrutura para tabela `login`
+-- Table structure for table `login`
 --
 
+DROP TABLE IF EXISTS `login`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `login` (
-  `idlogin` int(11) NOT NULL,
-  `usuario` varchar(150) NOT NULL,
-  `senha` varchar(150) NOT NULL
+  `idlogin` int NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(150) COLLATE utf8mb4_general_ci NOT NULL,
+  `senha` varchar(150) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`idlogin`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura stand-in para view `vwalunocarro`
--- (Veja abaixo para a visão atual)
---
-CREATE TABLE `vwalunocarro` (
-`Marca` varchar(50)
-,`Modelo` varchar(80)
-,`Ano` int(11)
-,`Aluno` varchar(100)
-,`Matricula` bigint(14)
-,`codigoEtiqueta` varchar(50)
-,`CNHvalida` tinyint(1)
-);
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estrutura para view `vwalunocarro`
+-- Table structure for table `logs`
 --
+
+DROP TABLE IF EXISTS `logs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `logs` (
+  `idlogs` int NOT NULL AUTO_INCREMENT,
+  `operacao` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `usuario` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `dataoperacao` datetime NOT NULL,
+  `detalhe` text COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`idlogs`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Temporary view structure for view `vwalunocarro`
+--
+
 DROP TABLE IF EXISTS `vwalunocarro`;
-
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vwalunocarro`  AS SELECT `c`.`marcaCarro` AS `Marca`, `c`.`modeloCarro` AS `Modelo`, `c`.`anoCarro` AS `Ano`, `a`.`noAluno` AS `Aluno`, `a`.`matriculaAluno` AS `Matricula`, `c`.`codigoEtiqueta` AS `codigoEtiqueta`, `c`.`validaCnh` AS `CNHvalida` FROM (`carro` `c` join `aluno` `a` on(`a`.`matriculaAluno` = `c`.`matriculaRel`)) ORDER BY `a`.`matriculaAluno` ASC ;
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices de tabela `aluno`
---
-ALTER TABLE `aluno`
-  ADD PRIMARY KEY (`matriculaAluno`);
-
---
--- Índices de tabela `carro`
---
-ALTER TABLE `carro`
-  ADD PRIMARY KEY (`idCarro`),
-  ADD KEY `fk_matricula` (`matriculaRel`);
+/*!50001 DROP VIEW IF EXISTS `vwalunocarro`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `vwalunocarro` AS SELECT 
+ 1 AS `Marca`,
+ 1 AS `Modelo`,
+ 1 AS `Ano`,
+ 1 AS `Aluno`,
+ 1 AS `Matricula`,
+ 1 AS `codigoEtiqueta`,
+ 1 AS `CNHvalida`*/;
+SET character_set_client = @saved_cs_client;
 
 --
--- Índices de tabela `login`
+-- Dumping events for database 'dbentrada'
 --
-ALTER TABLE `login`
-  ADD PRIMARY KEY (`idlogin`);
+/*!50106 SET @save_time_zone= @@TIME_ZONE */ ;
+/*!50106 DROP EVENT IF EXISTS `event_deleta_logs` */;
+DELIMITER ;;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;;
+/*!50003 SET character_set_client  = utf8mb4 */ ;;
+/*!50003 SET character_set_results = utf8mb4 */ ;;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;;
+/*!50003 SET @saved_time_zone      = @@time_zone */ ;;
+/*!50003 SET time_zone             = '+00:00' */ ;;
+/*!50106 CREATE*/ /*!50117 DEFINER=`root`@`localhost`*/ /*!50106 EVENT `event_deleta_logs` ON SCHEDULE EVERY 1 DAY STARTS '2023-06-26 19:31:12' ON COMPLETION NOT PRESERVE ENABLE DO DELETE FROM logs WHERE dataoperacao < NOW() - INTERVAL 30 DAY */ ;;
+/*!50003 SET time_zone             = @saved_time_zone */ ;;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;;
+/*!50003 SET character_set_results = @saved_cs_results */ ;;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;;
+DELIMITER ;
+/*!50106 SET TIME_ZONE= @save_time_zone */ ;
 
 --
--- AUTO_INCREMENT para tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `carro`
---
-ALTER TABLE `carro`
-  MODIFY `idCarro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- AUTO_INCREMENT de tabela `login`
---
-ALTER TABLE `login`
-  MODIFY `idlogin` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Restrições para tabelas despejadas
+-- Dumping routines for database 'dbentrada'
 --
 
 --
--- Restrições para tabelas `carro`
+-- Final view structure for view `vwalunocarro`
 --
-ALTER TABLE `carro`
-  ADD CONSTRAINT `fk_matricula` FOREIGN KEY (`matriculaRel`) REFERENCES `aluno` (`matriculaAluno`);
-COMMIT;
 
+/*!50001 DROP VIEW IF EXISTS `vwalunocarro`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `vwalunocarro` AS select `c`.`marcaCarro` AS `Marca`,`c`.`modeloCarro` AS `Modelo`,`c`.`anoCarro` AS `Ano`,`a`.`noAluno` AS `Aluno`,`a`.`matriculaAluno` AS `Matricula`,`c`.`codigoEtiqueta` AS `codigoEtiqueta`,`c`.`validaCnh` AS `CNHvalida` from (`carro` `c` join `aluno` `a` on((`a`.`matriculaAluno` = `c`.`matriculaRel`))) order by `a`.`matriculaAluno` */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-06-26 16:33:32
