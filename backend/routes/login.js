851ba2401c46d458a35fc37cedc68308300baf9b
kadetete <kadetesan@gmail.com>
2023-07-04 12:20:50 -0300

    sistema de login com encriptação no backend

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -16,3 +16,38 @@ con.connect(function(erroConexao) {
     }
 });
 
+router.post('/', function(req, res) {
+    const usuario = req.body.login;
+    const senha = req.body.senha;
+    
+    const sql = 'SELECT * FROM login WHERE usuario = ?'
+    con.query(sql, [usuario], function(erroComandoSQL, result) {
+        if (erroComandoSQL) {
+            console.error('Erro ao executar consulta:', erroComandoSQL);
+            res.status(500).send('Erro do servidor');
+            return;
+        }
+
+        if (result.length === 0) {
+            res.status(401).send('Credenciais inválidas');
+            return;
+        }
+
+        const user = result[0];
+
+        bcrypt.compare(senha, user.senha, function(erro, result) {
+            if (erro) {
+                console.error('Erro ao verificar senha:', erro);
+                res.status(500).send('Erro do servidor');
+                return;
+            }
+
+            if (!result) {
+                res.status(401).send('Credenciais inválidas');
+                return;
+            }
+
+            res.status(200).send('Login bem-sucedido')
+        });
+    });
+});
