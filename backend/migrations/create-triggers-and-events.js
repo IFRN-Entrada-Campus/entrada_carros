300a170133d0edd0a2fe1a77d016589c48c8f0d9
Guilherme Cadete <110258437+kadetete@users.noreply.github.com>
2023-07-18 18:28:23 +0000

    Inserindo triggers, eventose populando as tabelas por meio de migration

--- backend/migrations/create-triggers-and-events.js
+++ backend/migrations/create-triggers-and-events.js
@@ -4,56 +4,58 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     // Criação do trigger para a tabela 'aluno'
     await queryInterface.sequelize.query(`
-      DELIMITER $$
       CREATE TRIGGER after_aluno_insert AFTER INSERT ON aluno
       FOR EACH ROW
       BEGIN
         INSERT INTO logs (operacao, usuario, dataoperacao, detalhe)
         VALUES ('INSERT', 'root@localhost', NOW(), CONCAT('Registro inserido: matriculaAluno=', NEW.matriculaAluno, ' noAluno=', NEW.noAluno, '. Tabela aluno'));
-      END;
-      $$
+      END
+    `);
+
+    await queryInterface.sequelize.query(`
       CREATE TRIGGER after_aluno_update AFTER UPDATE ON aluno
       FOR EACH ROW
       BEGIN
         INSERT INTO logs (operacao, usuario, dataoperacao, detalhe)
         VALUES ('UPDATE', 'root@localhost', NOW(), CONCAT('Registro afetado: matriculaAluno=', OLD.matriculaAluno, ' noAluno=', OLD.noAluno, ' -> matriculaAluno=', NEW.matriculaAluno, ' noAluno=', NEW.noAluno, '. Tabela aluno'));
-      END;
-      $$
+      END
+    `);
+
+    await queryInterface.sequelize.query(`
       CREATE TRIGGER after_aluno_delete AFTER DELETE ON aluno
       FOR EACH ROW
       BEGIN
         INSERT INTO logs (operacao, usuario, dataoperacao, detalhe)
         VALUES ('DELETE', 'root@localhost', NOW(), CONCAT('Registro deletado: matriculaAluno=', OLD.matriculaAluno, ' noAluno=', OLD.noAluno, '. Tabela aluno'));
-      END;
-      $$
-      DELIMITER ;
+      END
     `);
 
     // Criação do trigger para a tabela 'carro'
     await queryInterface.sequelize.query(`
-      DELIMITER $$
       CREATE TRIGGER after_carro_insert AFTER INSERT ON carro
       FOR EACH ROW
       BEGIN
         INSERT INTO logs (operacao, usuario, dataoperacao, detalhe)
         VALUES ('INSERT', 'root@localhost', NOW(), CONCAT('Registro inserido: idCarro=', NEW.idCarro, ', marcaCarro=', NEW.marcaCarro, ', modeloCarro=', NEW.modeloCarro, ', anoCarro=', NEW.anoCarro, ', validaCnh=', NEW.validaCnh, ', codigoEtiqueta=', NEW.codigoEtiqueta, ', matriculaRel=', NEW.matriculaRel, '. Tabela carro'));
-      END;
-      $$
+      END
+    `);
+
+    await queryInterface.sequelize.query(`
       CREATE TRIGGER after_carro_update AFTER UPDATE ON carro
       FOR EACH ROW
       BEGIN
         INSERT INTO logs (operacao, usuario, dataoperacao, detalhe)
         VALUES ('UPDATE', 'root@localhost', NOW(), CONCAT('Registro afetado: idCarro=', OLD.idCarro, ', marcaCarro=', OLD.marcaCarro, ', modeloCarro=', OLD.modeloCarro, ', anoCarro=', OLD.anoCarro, ', validaCnh=', OLD.validaCnh, ', codigoEtiqueta=', OLD.codigoEtiqueta, ', matriculaRel=', OLD.matriculaRel, ' -> idCarro=', NEW.idCarro, ', marcaCarro=', NEW.marcaCarro, ', modeloCarro=', NEW.modeloCarro, ', anoCarro=', NEW.anoCarro, ', validaCnh=', NEW.validaCnh, ', codigoEtiqueta=', NEW.codigoEtiqueta, ', matriculaRel=', NEW.matriculaRel, '. Tabela carro'));
-      END;
-      $$
+      END
+    `);
+
+    await queryInterface.sequelize.query(`
       CREATE TRIGGER after_carro_delete AFTER DELETE ON carro
       FOR EACH ROW
       BEGIN
         INSERT INTO logs (operacao, usuario, dataoperacao, detalhe)
         VALUES ('DELETE', 'root@localhost', NOW(), CONCAT('Registro deletado: idCarro=', OLD.idCarro, ', marcaCarro=', OLD.marcaCarro, ', modeloCarro=', OLD.modeloCarro, ', anoCarro=', OLD.anoCarro, ', validaCnh=', OLD.validaCnh, ', codigoEtiqueta=', OLD.codigoEtiqueta, ', matriculaRel=', OLD.matriculaRel, '. Tabela carro'));
-      END;
-      $$
-      DELIMITER ;
+      END
     `);
 
     // Criação do evento para limpar registros antigos da tabela 'logs'
