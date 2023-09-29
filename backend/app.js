1133dca346b2c2b526ee3d783c78dd52e240773e
Guilherme Cadete <110258437+kadetete@users.noreply.github.com>
2023-09-29 12:02:42 +0000

    Divisão dos Dockerfiles e implementação do nginx

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+  })
+
 app.use('/', indexRouter);
 app.use('/alunocarro', alunocarroRouter);
 app.use('/login', loginRouter);
