8367358069872b2860ef2b698175e4c91977180e
kadetete <kadetesan@gmail.com>
2023-11-13 17:10:26 +0000

    implementação do scanner de QR code

--- frontend/src/app/scanner/scanner.component.ts
+++ frontend/src/app/scanner/scanner.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
-import { Result } from '@zxing/library';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { NgxScannerQrcodeComponent, ScannerQRCodeResult } from 'ngx-scanner-qrcode';
 
 @Component({
   selector: 'app-scanner',
   templateUrl: './scanner.component.html',
   styleUrls: ['./scanner.component.css']
 })
-export class ScannerComponent {
+export class ScannerComponent implements AfterViewInit {
+  @ViewChild('action') action!: NgxScannerQrcodeComponent;
+  valorDoScanner = '';
+
+  ngAfterViewInit(): void {
+    this.action.isReady.subscribe((res: any) => {
+
+    });
+  }
+
+  handleQrCodeResult(e: ScannerQRCodeResult[], action?: any): void {
+    action.pause();
+    this.valorDoScanner = e[0].value;
+  }
 }
 
