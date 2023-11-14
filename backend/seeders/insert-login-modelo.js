4f794bba8390ffc5307d79b64d16a5e53833eeab
kadetete <kadetesan@gmail.com>
2023-11-14 19:45:32 +0000

    fix: comentando inserção dos dados modelos usados para teste

--- backend/seeders/insert-login-modelo.js
+++ backend/seeders/insert-login-modelo.js
@@ -1,9 +1,62 @@
 'use strict';
 
+// module.exports = {
+//   up: async (queryInterface, Sequelize) => {
+
+//     const dataHoraInsercao = new Date('2023-12-31 23:59:59');
+//     return queryInterface.bulkInsert('carro', [
+//       {
+//         marcaCarro: 'Marca modelo',
+//         modeloCarro: 'Modelo 1',
+//         anoCarro: 1997,
+//         validaCnh: 1,
+//         codigoEtiqueta: 'ty1253267',
+//         validadeEtiqueta: dataHoraInsercao,
+//         matriculaRel: 12345678910,
+//         placaCarro: 'BRA2E19',
+//       },
+//       {
+//         marcaCarro: 'Marca modelo',
+//         modeloCarro: 'Modelo 1',
+//         anoCarro: 1997,
+//         validaCnh: 1,
+//         codigoEtiqueta: 'ty1253267',
+//         validadeEtiqueta: dataHoraInsercao,
+//         matriculaRel: 29876543210,
+//         placaCarro: 'RNO6T78',
+//       },
+//     ]);
+//   },
+
+//   down: async (queryInterface, Sequelize) => {
+//     return queryInterface.bulkDelete('carro', null, {});
+//   },
+// };
+
+// module.exports = {
+//   up: async (queryInterface, Sequelize) => {
+//     return queryInterface.bulkInsert('aluno', [
+//       {
+//         noAluno: 'Aluno modelo 1',
+//         matriculaAluno: 12345678910,
+//       },
+//       {
+//         noAluno: 'Aluno modelo 2',
+//         matriculaAluno: 29876543210,
+//       },
+//     ]);
+//   },
+
+//   down: async (queryInterface, Sequelize) => {
+//     return queryInterface.bulkDelete('aluno', null, {});
+//   },
+// };
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('login', [
       {
+        idlogin: 1,
         usuario: 'admin1',
         senha: '$2b$10$B48AwOk4psGv6zGvzkcbUeRLtVFTIdlp.FChRWKzuI8EkLs4bWffq'
       },
