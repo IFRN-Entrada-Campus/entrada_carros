bbb6613928b8288ce9f0cee04f6a8ab401a395f2
kadetete <kadetesan@gmail.com>
2024-03-13 16:41:48 +0000

    feat: transferência de arquivos com scp

--- scripts/backup_docker.sh
+++ scripts/backup_docker.sh
@@ -3,6 +3,9 @@
 # Diretório de backup
 BACKUP_DIR="/backup/docker"
 
+# Diretório de backup remoto
+REMOTE_BACKUP_DIR=""
+
 # Obter a lista de contêineres em execução
 CONTAINERS=$(docker ps -q)
 
@@ -15,7 +18,10 @@ do
     # Realizar o backup do contêiner
     docker export $CONTAINER_ID | gzip > $BACKUP_FILE
 
-    echo "Backup do contêiner $CONTAINER_NAME concluído: $BACKUP_FILE"
+    # Copiar o backup para o diretório remoto usando scp
+    scp $BACKUP_FILE $REMOTE_BACKUP_DIR
+
+    echo "Backup do contêiner $CONTAINER_NAME concluído e enviado para a máquina remota: $BACKUP_FILE"
 done
 
 echo "Backup concluído em $(date +"%Y-%m-%d %H:%M:%S")"
--- scripts/backup_sql.sh
+++ scripts/backup_sql.sh
@@ -9,11 +9,17 @@ DB_HOST="localhost"  # ou o endereço IP do container, dependendo do seu ambient
 # Diretório de backup
 BACKUP_DIR="/backup/sql"
 
+# Diretório de backup remoto
+REMOTE_BACKUP_DIR=""
+
 # Nome do arquivo de backup
 BACKUP_FILE="${BACKUP_DIR}/backup_$(date '+%Y%m%d%H%M%S').sql"
 
 # Comando mysqldump para criar o backup
 docker exec -i db mysqldump -u$DB_USER -p$DB_PASSWORD -h$DB_HOST --complete-insert $DB_NAME > $BACKUP_FILE
+
+# Copiar o backup para o diretório remoto usando scp
+scp $BACKUP_FILE $REMOTE_BACKUP_DIR
 
 # Verificar se o backup foi criado com sucesso
 if [ $? -eq 0 ]; then
