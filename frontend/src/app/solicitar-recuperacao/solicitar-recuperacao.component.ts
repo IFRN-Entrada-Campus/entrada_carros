fe51967103475cc6cd69fa4bd5ee83ce4bdf0107
Augusto240 <jose.aug18@outlook.com>
2024-11-25 10:46:39 -0300

    criando a lógica de solicitar recuperação no componente de solicitar recuperação de senha

--- frontend/src/app/solicitar-recuperacao/solicitar-recuperacao.component.ts
+++ frontend/src/app/solicitar-recuperacao/solicitar-recuperacao.component.ts
@@ -3,6 +3,8 @@ import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
+import { SolicitarRecuperacaoService } from '../solicitar-recuperacao.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-solicitar-recuperacao',
@@ -11,7 +13,24 @@ import { RippleModule } from 'primeng/ripple';
   styleUrls: ['./solicitar-recuperacao.component.css'],
   imports: [FormsModule, InputTextModule, ButtonModule, RippleModule]
 })
+
 export class SolicitarRecuperacaoComponent {
-  email = '';
-  constructor() { }
+  email: string = '';  // A variável que vai armazenar o e-mail do usuário
+  mensagemErro: string = ''; // variável para armazenar mensagens de erro
+  mensagemSucesso: string = ''; //variável para armazenar mensagens de sucesso
+
+  constructor(private solicitarRecuperacaoService: SolicitarRecuperacaoService, private router: Router) { }
+
+  solicitarRecuperacao() {
+    this.solicitarRecuperacaoService.solicitarCodigoRecuperacao(this.email).subscribe(
+      (response) => {
+        this.mensagemSucesso = response.message;
+        this.mensagemErro = '';  // Limpa mensagens de erro
+      },
+      (error) => {
+        this.mensagemErro = error.error.message || 'Erro ao enviar o código de recuperação';
+        this.mensagemSucesso = '';  // Limpa mensagens de sucesso
+      }
+    );
+  }
 }
